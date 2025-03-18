5dae5cc2065c4f5c42c9c1dd7ab6f8250f82253d
benzaaar <benzar245@gmail.com>
2025-03-18 00:20:26 -0400

    gulp scaffold according to the gulp template repo

---
+++ gulpfile.js
@@ -0,0 +1,136 @@
+const { src, dest, series, watch } = require(`gulp`),
+    CSSLinter = require(`gulp-stylelint`),
+    babel = require(`gulp-babel`),
+    htmlCompressor = require(`gulp-htmlmin`),
+    htmlValidator = require(`gulp-html`),
+    imageCompressor = require(`gulp-image`),
+    jsCompressor = require(`gulp-uglify`),
+    jsLinter = require(`gulp-eslint`),
+    sass = require(`gulp-sass`)(require(`sass`)),
+    browserSync = require(`browser-sync`),
+    reload = browserSync.reload;
+
+let browserChoice = `default`;
+
+let validateHTML = () => {
+    return src([`*.html`, `**/*.html`])
+        .pipe(htmlValidator(undefined));
+};
+
+let compileCSSForDev = () => {
+    return src(`styles/main.css`)
+        .pipe(sass.sync({
+            outputStyle: `expanded`,
+            precision: 10
+        }).on(`error`, sass.logError))
+        .pipe(dest(`temp/styles`));
+};
+
+let lintJS = () => {
+    return src(`js/*.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
+let transpileJSForDev = () => {
+    return src(`js/*.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/scripts`));
+};
+
+let compressHTML = () => {
+    return src([`*.html`, `**/*.html`])
+        .pipe(htmlCompressor({collapseWhitespace: true}))
+        .pipe(dest(`prod`));
+};
+
+let compileCSSForProd = () => {
+    return src(`styles/main.css`)
+        .pipe(sass.sync({
+            outputStyle: `compressed`,
+            precision: 10
+        }).on(`error`, sass.logError))
+        .pipe(dest(`prod/styles`));
+};
+
+let transpileJSForProd = () => {
+    return src(`js/*.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/scripts`));
+};
+
+let compressImages = () => {
+    return src(`img/**/*`)
+        .pipe(imageCompressor({
+            optipng: [`-i 1`, `-strip all`, `-fix`, `-o7`, `-force`],
+            pngquant: [`--speed=1`, `--force`, 256],
+            zopflipng: [`-y`, `--lossy_8bit`, `--lossy_transparent`],
+            jpegRecompress: [`--strip`, `--quality`, `medium`, `--min`, 40,
+                `--max`, 80],
+            mozjpeg: [`-optimize`, `-progressive`],
+            gifsicle: [`--optimize`],
+            svgo: [`--enable`, `cleanupIDs`, `--disable`, `convertColors`],
+            quiet: false
+        }))
+        .pipe(dest(`prod/img`));
+};
+
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        browser: browserChoice,
+        server: {
+            baseDir: [
+                `temp`,
+                `.`
+            ]
+        }
+    });
+
+    watch(`js/*.js`, series(lintJS, transpileJSForDev))
+        .on(`change`, reload);
+
+    watch(`styles/**/*.css`, compileCSSForDev)
+        .on(`change`, reload);
+
+    watch(`*.html`, validateHTML)
+        .on(`change`, reload);
+
+    watch(`img/**/*`)
+        .on(`change`, reload);
+};
+
+let lintCSS = () => {
+    return src(`styles/**/*.css`)
+        .pipe(CSSLinter({
+            failAfterError: false,
+            reporters: [
+                {formatter: `string`, console: true}
+            ]
+        }));
+};
+
+exports.validateHTML = validateHTML;
+exports.compileCSSForDev = compileCSSForDev;
+exports.lintJS = lintJS;
+exports.transpileJSForDev = transpileJSForDev;
+exports.compressHTML = compressHTML;
+exports.compileCSSForProd = compileCSSForProd;
+exports.transpileJSForProd = transpileJSForProd;
+exports.compressImages = compressImages;
+exports.lintCSS = lintCSS;
+exports.serve = series(
+    validateHTML,
+    compileCSSForDev,
+    lintJS,
+    transpileJSForDev,
+    serve
+);
+exports.build = series(
+    compressHTML,
+    compileCSSForProd,
+    transpileJSForProd,
+    compressImages
+);
